cb7cf2bc18591c909bd0422dc0097fc9f8c0d6a8
AndersonNobrega <anderson.nobrega62@gmail.com>
2021-10-07 00:03:06 -0300

    Adding support to args and ECO multiple buildings conversion support

--- src/converters/eco/converter.py
+++ src/converters/eco/converter.py
@@ -1,18 +1,23 @@
-from multiprocessing import Process
+from multiprocessing import cpu_count, Pool
 from nilmtk.elecmeter import ElecMeter
 from nilmtk.metergroup import MeterGroup
 from nilmtk import DataSet
-from numpy import empty, copy
+from numpy import empty
 from pathlib import Path
 from pickle import dump
 from os import path
 
+import json
+import logging
+
 
 class EcoConverter():
-    def __init__(self, dir_path, h5_file='/data/h5/ECO.h5', dat_path='/data/dat/eco'):
+    def __init__(self, dir_path, h5_file='/data/h5/ECO.h5', dat_path='/data/dat/eco', measuraments=3, features=1):
         self.h5_file = h5_file
         self.dat_path = dat_path
         self.dir_path = dir_path
+        self.measuraments = measuraments
+        self.features = features
 
     def read_dataset(self, file):
         if Path(file).is_file():
@@ -43,11 +48,13 @@ class EcoConverter():
         return values
 
     def create_dat_file(self, df, df_aggregate, file_name, building_name):
-        if not path.isdir(self.dir_path + self.dat_path + building_name):
-            Path(self.dir_path + self.dat_path + building_name).mkdir(parents=True, exist_ok=True)
-        dat_file = open(self.dir_path + self.dat_path + building_name + '/' + file_name, 'wb')
+        base_path = self.dir_path + self.dat_path + building_name
+        if not path.isdir(base_path):
+            Path(base_path).mkdir(parents=True, exist_ok=True)
 
-        file_values = empty((len(df), 3, 1))
+        dat_file = open(base_path + '/' + file_name, 'wb')
+
+        file_values = empty((len(df), self.measuraments, self.features))
 
         for i, row in enumerate(df.itertuples()):
             file_values[i][0] = [row.Index.to_pydatetime().timestamp()]
@@ -58,29 +65,134 @@ class EcoConverter():
         dump(file_values, dat_file)
         dat_file.close()
 
+        appliance_name = path.splitext(file_name)[0]
+        self.create_metadata(building_name, appliance_name, len(df), (base_path + '/' + appliance_name + '.json'))
+
     def run_processes(self, df_list, df_aggregate, building_name):
-        processes = list()
+        pool = Pool(processes=cpu_count() // 3)
         for df, file_name in df_list:
-            new_process = Process(target=self.create_dat_file, args=(df, df_aggregate, file_name, building_name))
-            processes.append(new_process) # Save process to join later
-            new_process.start()
+            pool.apply_async(self.create_dat_file, args=(self.read_df(df), df_aggregate, file_name, building_name))
+        pool.close()
+        pool.join()
 
-        for process in processes:
-            process.join()
+    def create_metadata(self, building, appliance, data_len, path):
+        data = {
+            'building': building,
+            'appliance': appliance,
+            'data_len': data_len,
+        }
 
-    def convert_df(self):
-        elec = self.read_dataset(self.dir_path + self.h5_file).buildings[1].elec
+        with open(path, 'w') as outfile:
+            json.dump(data, outfile)
 
-        df_aggregate = next(elec.mains().load())
+    def convert_df(self, buildings_list):
+        for value in buildings_list:
+            if value == 1:
+                logging.info('Converting data from building 1...')
+                elec = self.read_dataset(self.dir_path + self.h5_file).buildings[1].elec
+                df_aggregate = next(elec.mains().load())
 
-        df_list = [
-            [self.read_df(elec[4]), 'fridge.dat'],
-            [self.read_df(elec[5]), 'hair_dryer.dat'],
-            [self.read_df(elec[6]), 'coffee_maker.dat'],
-            [self.read_df(elec[7]), 'kettle.dat'],
-            [self.read_df(elec[8]), 'washing_machine.dat'],
-            [self.read_df(elec[9]), 'computer.dat'],
-            [self.read_df(elec[10]), 'freezer.dat'],
-        ]
+                df_building = [
+                    [elec[4], 'fridge.dat'],
+                    [elec[5], 'hair_dryer.dat'],
+                    [elec[6], 'coffee_maker.dat'],
+                    [elec[7], 'kettle.dat'],
+                    [elec[8], 'washing_machine.dat'],
+                    [elec[9], 'computer.dat'],
+                    [elec[10], 'freezer.dat'],
+                ]
 
-        self.run_processes(df_list, df_aggregate, '/building1')
+                self.run_processes(df_building, df_aggregate, '/building1')
+                logging.info('Conversion from building 1 finished.')
+            elif value == 2:
+                logging.info('Converting data from building 2...')
+                elec = self.read_dataset(self.dir_path + self.h5_file).buildings[2].elec
+                df_aggregate = next(elec.mains().load())
+
+                df_building = [
+                    [elec[4], 'tablet_computer_charger.dat'],
+                    [elec[5], 'dish_washer.dat'],
+                    [elec[6], 'air_handling_unit.dat'],
+                    [elec[7], 'fridge.dat'],
+                    [elec[8], 'HTPC.dat'],
+                    [elec[9], 'freezer.dat'],
+                    [elec[10], 'kettle.dat'],
+                    [elec[11], 'lamp.dat'],
+                    [elec[12], 'laptop_computer.dat'],
+                    [elec[13], 'stove.dat'],
+                    [elec[14], 'television.dat'],
+                    [elec[15], 'audio_system.dat'],
+                ]
+
+                self.run_processes(df_building, df_aggregate, '/building2')
+                logging.info('Conversion from building 2 finished.')
+            elif value == 3:
+                logging.info('Converting data from building 3...')
+                elec = self.read_dataset(self.dir_path + self.h5_file).buildings[3].elec
+                df_aggregate = next(elec.mains().load())
+
+                df_building = [
+                    [elec[4], 'laptop_computer.dat'],
+                    [elec[5], 'freezer.dat'],
+                    [elec[6], 'coffee_maker.dat'],
+                    [elec[7], 'computer.dat'],
+                    [elec[8], 'fridge.dat'],
+                    [elec[9], 'kettle.dat'],
+                    [elec[10], 'HTPC.dat'],
+                ]
+
+                self.run_processes(df_building, df_aggregate, '/building3')
+                logging.info('Conversion from building 3 finished.')
+            elif value == 4:
+                logging.info('Converting data from building 4...')
+                elec = self.read_dataset(self.dir_path + self.h5_file).buildings[4].elec
+                df_aggregate = next(elec.mains().load())
+
+                df_building = [
+                    [elec[4], 'fridge.dat'],
+                    [elec[5], 'small_cooking_appliance.dat'],
+                    [elec[6], 'lamp.dat'],
+                    [elec[7], 'laptop_computer.dat'],
+                    [elec[8], 'freezer.dat'],
+                    [elec[9], 'games_console.dat'],
+                    [elec[10], 'HTPC.dat'],
+                    [elec[11], 'microwave.dat'],
+                ]
+
+                self.run_processes(df_building, df_aggregate, '/building4')
+                logging.info('Conversion from building 4 finished.')
+            elif value == 5:
+                logging.info('Converting data from building 5...')
+                elec = self.read_dataset(self.dir_path + self.h5_file).buildings[5].elec
+                df_aggregate = next(elec.mains().load())
+
+                df_building = [
+                    [elec[4], 'laptop_computer.dat'],
+                    [elec[5], 'coffee_maker.dat'],
+                    [elec[6], 'garden_sprinkler.dat'],
+                    [elec[7], 'microwave.dat'],
+                    [elec[8], 'fridge.dat'],
+                    [elec[9], 'HTPC.dat'],
+                    [elec[10], 'computer.dat'],
+                    [elec[11], 'kettle.dat'],
+                ]
+
+                self.run_processes(df_building, df_aggregate, '/building5')
+                logging.info('Conversion from building 5 finished.')
+            elif value == 6:
+                logging.info('Converting data from building 6...')
+                elec = self.read_dataset(self.dir_path + self.h5_file).buildings[6].elec
+                df_aggregate = next(elec.mains().load())
+
+                df_building = [
+                    [elec[4], 'lamp.dat'],
+                    [elec[5], 'laptop_computer.dat'],
+                    [elec[6], 'broadband_router.dat'],
+                    [elec[7], 'coffee_maker.dat'],
+                    [elec[8], 'HTPC.dat'],
+                    [elec[9], 'fridge.dat'],
+                    [elec[10], 'kettle.dat'],
+                ]
+
+                self.run_processes(df_building, df_aggregate, '/building6')
+                logging.info('Conversion from building 6 finished.')
--- src/main.py
+++ src/main.py
@@ -1,16 +1,34 @@
-from os import getcwd, path
 from converters.ampds.converter import AmpdsConverter
 from converters.eco.converter import EcoConverter
+from os import getcwd, path
+
+import argparse
+import logging
 
 def main():
-    eco = EcoConverter(dir_path=path.realpath(path.join(getcwd(), path.dirname(__file__)))[:-4])
+    logging.getLogger().setLevel(logging.INFO)
 
-    #ampds = AmpdsConverter(dir_path=path.realpath(path.join(getcwd(), path.dirname(__file__)))[:-4])
+    parser = argparse.ArgumentParser(allow_abbrev=False, description='Converts NILMTK h5 files to dat for use with WaveNILM.')
 
-    eco.convert_df()
-    #ampds.convert_df()
+    parser.add_argument('-a', '--ampds', action='store_true', help='Enables conversion for AMPDS dataset.')
+    parser.add_argument('-e', '--eco', nargs='+', type=int, help='Enables conversion for AMPDS dataset. Needs to specify buildings.')
+    args = vars(parser.parse_args())
+    
+    if args['ampds']:
+        logging.info('Starting AMPDS dataset conversion.')
+        AmpdsConverter(dir_path=path.realpath(path.join(getcwd(), path.dirname(__file__)))[:-4]).convert_df()
+        logging.info('AMPDS dataset conversion completed.')
+    
+    if args['eco']:
+        logging.info('Starting ECO dataset conversion.')
+        EcoConverter(dir_path=path.realpath(path.join(getcwd(), path.dirname(__file__)))[:-4]).convert_df(args['eco'])
+        logging.info('ECO dataset conversion completed.')
 
     exit(0)
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except MemoryError:
+        logging.error('Memory Exception')
+        exit(1)
